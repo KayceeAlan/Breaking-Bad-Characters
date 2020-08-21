9ee7245e0ab3b9a932c9d11d4f5bfad7dfc42bf0
Kelechi Alan <68669988+KayceeAlan@users.noreply.github.com>
2020-08-21 01:49:34 +0100

    Add files via upload

---
+++ BreakingBadBuild/precache-manifest.bf11c7d96e0653519d2a48d2ff33e1c6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c767afd84c8eb1199734f4a1f271812e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe0544d34d6d93fd82fa",
+    "url": "/static/css/main.aceea456.chunk.css"
+  },
+  {
+    "revision": "c1c8b65ebbdc652ba9e0",
+    "url": "/static/js/2.993b278f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.993b278f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe0544d34d6d93fd82fa",
+    "url": "/static/js/main.9c212ed9.chunk.js"
+  },
+  {
+    "revision": "e511cb0245e5a6f42eea",
+    "url": "/static/js/runtime-main.fea54d51.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/static/media/spinner.235b9b3e.gif"
+  }
+]);
